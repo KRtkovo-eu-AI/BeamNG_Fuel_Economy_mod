9828bcb6669d95442bda974f44288e7c0d74bbe7
Ondřej Kotas <ok@krtkovo.eu>
2025-09-08 22:49:56 +0200

    feat: log game pause state

--- okFuelEconomy/lua/ge/extensions/okGameState.lua
+++ okFuelEconomy/lua/ge/extensions/okGameState.lua
@@ -1,13 +1,33 @@
 local M = {}
 
+local lastPaused
+
+local function isPaused()
+  return be:getPlayState() ~= 1
+end
+
+local function logIfChanged(paused)
+  if paused ~= lastPaused then
+    lastPaused = paused
+    log('I', 'okGameState', paused and 'Game paused' or 'Game resumed')
+  end
+end
+
 local function getState()
-  return { paused = be:getPlayState() ~= 1 }
+  return { paused = isPaused() }
+end
+
+local function checkPaused()
+  logIfChanged(isPaused())
 end
 
 M.onInit = function()
   if streams and streams.registerStream then
     streams.registerStream('okGameState', getState)
   end
+  checkPaused()
 end
+
+M.onUpdate = checkPaused
 
 return M
