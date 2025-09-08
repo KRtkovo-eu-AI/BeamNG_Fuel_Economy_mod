1f4d6460a3f1d1163e2d0d67de87d4d98e0a8986
Ondřej Kotas <ok@krtkovo.eu>
2025-09-09 00:19:21 +0200

    Use pause hooks for reliable detection

--- okFuelEconomy/lua/ge/extensions/okGameState.lua
+++ okFuelEconomy/lua/ge/extensions/okGameState.lua
@@ -1,38 +1,35 @@
 local M = {}
 
-local lastPaused
+local paused
 
-local function isPaused()
-  -- be:getPlayState is not available in all environments, so guard against
-  -- calling a nil method which would raise a Lua error inside the game
-  if be and be.getPlayState then
-    return be:getPlayState() ~= 1
-  end
-  return false
-end
-
-local function logIfChanged(paused)
-  if paused ~= lastPaused then
-    lastPaused = paused
-    log('I', 'okGameState', paused and 'Game paused' or 'Game resumed')
+local function setPaused(state)
+  if state ~= paused then
+    paused = state
+    log('I', 'okGameState', state and 'Game paused' or 'Game resumed')
   end
 end
 
 local function getState()
-  return { paused = isPaused() }
-end
-
-local function checkPaused()
-  logIfChanged(isPaused())
+  return { paused = paused }
 end
 
 M.onExtensionLoaded = function()
   if streams and streams.registerStream then
     streams.registerStream('okGameState', getState)
   end
-  checkPaused()
+  if be and be.getPlayState then
+    setPaused(be:getPlayState() ~= 1)
+  else
+    setPaused(false)
+  end
 end
 
-M.onUpdate = checkPaused
+M.onPhysicsPaused = function()
+  setPaused(true)
+end
+
+M.onPhysicsUnpaused = function()
+  setPaused(false)
+end
 
 return M
